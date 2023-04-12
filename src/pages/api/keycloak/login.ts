88e33547bfcad577829a99a44c527b8bc697260e
ellenjane001 <ellenjane.000001998@gmail.com>
2023-04-12 17:32:03 +0800

    added keycloak token generator

--- src/pages/api/keycloak/login.ts
+++ src/pages/api/keycloak/login.ts
@@ -1,19 +1,24 @@
 import axios from "axios";
 import { NextApiRequest, NextApiResponse } from "next";
+import qs from 'qs'
 
 export default async function handler(req: NextApiRequest, res: NextApiResponse) {
-    // try {
-        const keycloakUser = await axios.post(`${process.env.KEYCLOAK_TOKEN_GEN}`,
-            [{
-                "grant_type": "password",
-                "username": "ellen",
-                "password": "ellen",
-                "client_id": "admin-cli"
-            }], { headers: { "Content-Type": "application/x-www-form-urlencoded" } })
-
-        return res.status(200).json({ ...keycloakUser })
-    // }
-    // catch (e) {
-    //     return res.status(400).json(e)
-    // }
+    try {
+        let data = qs.stringify({
+            grant_type: "password",
+            username: "ellen",
+            password: "ellen",
+            client_id: "admin-cli"
+        })
+        const keycloakUser = await axios.request({
+            url: `${process.env.KEYCLOAK_TOKEN_GEN}`,
+            method: "POST",
+            data,
+            headers: { "Content-Type": "application/x-www-form-urlencoded" }
+        })
+        return res.status(200).json(keycloakUser.data)
+    }
+    catch (e) {
+        return res.status(400).json(e)
+    }
 }
